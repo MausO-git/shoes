82d93e7ead082901b51d4d3dc69116861ffa9cdd
Oscar Jean-Francois Maus <mauso@classes.local>
2025-09-11 09:40:00 +0200

    404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+    header("HTTP/1.0 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page introuvable !!!</h1>
+    <a href="last">Revenir à l'acceuil</a>
+</body>
+</html>
